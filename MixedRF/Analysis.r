65a4581d490b5b3f51eeb2602c455a8e491a02fb
JeeHun Sung <jhun1023@naver.com>
2022-09-12 14:32:59 +0900

    sharing

--- MixedRF/Analysis.r
+++ MixedRF/Analysis.r
@@ -1,47 +1,59 @@
 library('haven') 
-library('randomForest') 
-library('dplyr')
 library('readxl')
+library('dplyr')
+library('randomForest') 
+
 
 set.seed(41) # set random seed
-NA2mean <- function(x) replace(x, is.na(x), mean(x, na.rm = TRUE))
+# NA2mean <- function(x) replace(x, is.na(x), mean(x, na.rm = TRUE))
 
 
-df <- read.csv('/Users/huni/Dropbox/[0]groundground/2021 random Forest/0220/DATA/Individual data_cleaningdone_v2.csv')
+###
+# load and slicing data will be converted to function
+###
+df <- read_excel('C:\\Users\\jhun1\\Proj\\Research\\MixedRF\\data\\preprocessing.xlsx', sheet='full')
+df$resilient <- as.factor(df$resilient)
 
-df_SK <- df[!(df$CNT=='Korea'),]
-df_US <- df[!(df$CNT=='United State')]
+df_SK <- df[df$CNT=='Korea', ]
+df_US <- df[df$CNT=='United States', ]
 
-df_SK <- df_SK[-c(1,2,3)] # index, ID, gender #!# to be checked
+df_SK <- df_SK[-c(1,2,3)] # CNT, CNTSCHID, CNTSTUID
 df_US <- df_US[-c(1,2,3)]
 
-
+print(dim(df_SK))
+print(dim(df_US))
 
 ###
 # Start Random Forest
 ####
 doRandomForest <- function(inputDf, title) {
-  train_idx <- sample(1:nrow(inputDf), size=0.7*nrow(inputDf), replace=F)
-  test_idx <- -train_idx
+  inputDf[sapply(inputDf, is.character)] <- lapply(inputDf[sapply(inputDf, is.character)], 
+                                               as.factor)
+  df.roughfix <- na.roughfix(inputDf)
   
-  df_train <- inputDf[train_idx, ]
-  df_test <- df_IB[test_idx, ]
+  train_idx <- sample(1:nrow(df.roughfix), size=0.7*nrow(df.roughfix), replace=F)
   
-  rf <- randomForest(resilient ~.,
-                     data = df_train,
-                     mtry = floor(ncol(inputDf)/3), #!# it's categorical problem
-                     ntree = 1000 #!# value should be checked
-  )
-  rf.pred <- predict(rf, newData = df_test)
-  rf.pred.train <- predict(rf, newData = df_train)
-  plot(x = rf.pred, y= df_test$resilient)
+  df_train <- df.roughfix[train_idx, ]
+  df_test <- df.roughfix[-train_idx, ]
+  print(dim(df_test))
+  
+  rf <- randomForest(resilient ~., data = df_train, mtry = floor(sqrt(ncol(df_train))), ntree = 10)
+                     # do.trace = TRUE # if set True, console window get dirty
+
+  pred <- predict(object = rf, newData = df_test, type='class', se=T)
+  print(length(pred))
+  # print(pred)
+  plot(x = pred, y= df_test$resilient)
+
+  rf.pred.train <- predict(object= rf, newData = df_train)
   
   # cor.test #!# TBD
-  varImpPlot(rf, main=title)
-  importance(rf)
+  # varImpPlot(rf, main=title)
+  # importance(rf)
 }
 
-doRandomForest(df, title='full data')
+# doRandomForest(df, title='full data')
+str(df_SK)
 doRandomForest(df_SK, title='south korea')
 doRandomForest(df_US, title='us')
 
